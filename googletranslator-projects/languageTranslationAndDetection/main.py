c61627a37f0bdcf048bb6301a90c74d893c6ded8
vibhatsu08 <mistryvedant17@gmail.com>
2023-01-31 01:01:50 +0530

    Tested out detection, transation of text from one language to the other using the source and destination parameters in the translate method.

--- googletranslator-projects/languageTranslationAndDetection/main.py
+++ googletranslator-projects/languageTranslationAndDetection/main.py
@@ -1,5 +1,27 @@
 from googletrans import Translator
+
+# made an instance of the Translator
 translator = Translator()
-text1 = "hello world"
-dt1 = translator.detect(text1)
-print(dt1)
+
+# simple detection
+test1 = "hello world"
+dtest1 = translator.detect(test1)
+print(dtest1)
+
+# simple translation
+test2 = "namaste duniya"
+ttest2 = translator.translate(test2)
+# .text is important, because it prints the raw version of the translation, raw meaning, it prints the translation along with the source, detected language, translated language and many other.
+print(ttest2.text)
+
+# now using the source and destination parameters for the translation
+test3 = "i am a programmer"
+ttest3 = translator.translate(test3, src="en", dest="hi")
+print(ttest3.text)
+
+# translating a list of texts
+test4 = ["hello world", "my name is albert", "i am a programmer"]
+for test in test4 :
+    ttest4 = translator.translate(test, src="en", dest="hi")
+    print(ttest4.text)
+    
